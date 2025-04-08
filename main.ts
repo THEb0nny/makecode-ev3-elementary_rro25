3dea6a9f638eaf5a6dadfc45d0be14dd496071bd
Dmitriy Antipov <aston_bonny@mail.ru>
2025-04-09 01:21:16 +0300

    Обновить main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,16 @@ function TransferBattery () {
     chassis.rampLinearDistMove(40, 70, 150, 60, 100)
     Claw(false, 50, false)
     pause(50)
-    chassis.linearDistMove(70, -60, Braking.Hold)
-    pause(50)
+    chassis.linearDistMove(60, -60, Braking.Hold)
+    pause(100)
     chassis.spinTurn(90, 70)
+    pause(100)
     RaiseClawAfterDelayInParallel(3000)
     chassis.rampLinearDistMove(30, 60, 120, 60, 40)
-    motions.rampLineFollowToDistance(1670, 100, 0, Braking.NoStop, params.rampLineFollowSixParams(30, 70, 70, 0.6, 0.8))
+    pause(100)
+    levelings.linePositioning(500, params.linePositioningAllParams(500, 50, 0.6, 0, 0))
+    pause(200)
+    motions.rampLineFollowToDistance(1670, 150, 0, Braking.NoStop, params.rampLineFollowSixParams(30, 70, 70, 0.6, 0.8))
     chassis.rampLinearDistMove(30, 70, 100, 0, 70)
     chassis.linearDistMove(80, -60, Braking.Hold)
     pause(50)
@@ -21,19 +25,19 @@ function MoveTwoRedSpaceGarbage () {
         motions.lineFollowToSideIntersection(SideIntersection.RightInside, AfterMotion.BreakStop, params.lineFollowTwoParams(40, 0.9))
         motions.rampLineFollowToDistance(240, 0, 100, Braking.Hold, params.rampLineFollowThreeParams(40, 50, 30))
     } else {
-        motions.rampLineFollowToDistance(390, 100, 100, Braking.Hold, params.rampLineFollowThreeParams(30, 50, 30))
+        motions.rampLineFollowToDistance(380, 100, 100, Braking.Hold, params.rampLineFollowThreeParams(30, 50, 30))
     }
     chassis.spinTurn(90, 70)
     levelings.lineAlignment(VerticalLineLocation.Behind, 1000, false, params.lineAlignmentSevenParams(50, 1.1, 1.1, 0, 0))
     chassis.rampLinearDistMove(40, 80, 660, 40, 70)
-    Claw(false, 50, false)
+    Claw(false, 40, false)
     chassis.linearDistMove(300, -60, Braking.Hold)
     pause(100)
     chassis.spinTurn(-90, 70)
     pause(50)
     Claw(true, 60, true)
     chassis.rampLinearDistMove(40, 80, 150, 40, 70)
-    Claw(false, 50, false)
+    Claw(false, 40, false)
     pause(50)
     chassis.linearDistMove(150, -60, Braking.Hold)
     pause(100)
@@ -83,7 +87,7 @@ function CheckColor (debug: boolean) {
 }
 // Часть 3 - перевести первый спутник
 function MoveSatellite () {
-    motions.rampLineFollowToDistance(100, 60, 40, Braking.Hold, params.rampLineFollowThreeParams(30, 60, 30))
+    motions.rampLineFollowToDistance(100, 60, 40, Braking.Hold, params.rampLineFollowThreeParams(40, 60, 30))
     pause(50)
     chassis.spinTurn(90, 60)
     levelings.lineAlignment(VerticalLineLocation.Behind, 750, false, params.lineAlignmentSevenParams(50, 1.1, 1.1, 0, 0))
@@ -92,22 +96,23 @@ function MoveSatellite () {
     chassis.pivotTurn(90, 70, WheelPivot.LeftWheel)
     pause(50)
     chassis.linearDistMove(70, 50, Braking.Hold)
+    current_color = -1
     for (let index = 0; index <= 4; index++) {
         setellite_zone = index + 1
-        Claw(false, 60, false)
+        Claw(false, 20, false)
         current_color = CheckSetelliteColor()
-        if (current_color != ColorSensorColor.None) {
-            sattelites[setellite_zone] = current_color
+        brick.printValue("2current_coolor", current_color, 12)
+        if (current_color != 0) {
             VoiceSatelliteColor(current_color)
             break;
         } else {
-            music.playSoundEffect(sounds.communicationNo)
+            music.playSoundEffectUntilDone(sounds.communicationNo)
             Claw(true, 75, true)
             chassis.spinTurn(5, 50)
             chassis.linearDistMove(150, 50, Braking.Hold)
         }
     }
-    pause(100)
+    pause(50)
     chassis.linearDistMove(20, -60, Braking.Hold)
     chassis.spinTurn(-90, 80)
     motions.moveToRefZone(0, 50, LineSensorSelection.LeftAndRight, Comparison.Less, 20, AfterMotion.BreakStop)
@@ -117,24 +122,25 @@ function MoveSatellite () {
     chassis.spinTurn(90, 60)
     motions.rampLineFollowToDistance(900 - 150 * (setellite_zone - 1), 150, 0, Braking.NoStop, params.rampLineFollowSixParams(30, 70, 50, 0.45, 0.5))
     motions.lineFollowToSideIntersection(SideIntersection.LeftInside, AfterMotion.DecelRolling, params.lineFollowTwoParams(50, 0.8))
-    chassis.spinTurn(-90, 50)
     pause(50)
-    motions.lineFollowToCrossIntersection(AfterMotion.NoStop, params.lineFollowFourParams(80, 0.5, 0.5))
+    chassis.spinTurn(-90, 60)
+    pause(50)
+    motions.lineFollowToCrossIntersection(AfterMotion.NoStop, params.lineFollowFourParams(70, 0.5, 0.5))
     chassis.rampLinearDistMove(30, 80, 280, 50, 70)
     pause(50)
     chassis.spinTurn(-90, 60)
     pause(50)
-    chassis.rampLinearDistMove(30, 80, 320, 50, 100)
+    chassis.rampLinearDistMove(40, 80, 310, 50, 100)
     pause(50)
-    if (current_color == ColorSensorColor.White) {
+    if (current_color == 6) {
         cross = 0
-    } else if (current_color == ColorSensorColor.Green) {
+    } else if (current_color == 3) {
         cross = 1
-    } else if (current_color == ColorSensorColor.Blue) {
+    } else if (current_color == 2) {
         cross = 2
-    } else if (current_color == ColorSensorColor.Yellow) {
+    } else if (current_color == 4) {
         cross = 3
-    } else if (current_color == ColorSensorColor.Red) {
+    } else if (current_color == 5) {
         cross = 4
     }
     if (false) {
@@ -154,23 +160,32 @@ function MoveSatellite () {
     Claw(true, 75, true)
     chassis.linearDistMove(80, -50, Braking.Hold)
 }
+brick.buttonLeft.onEvent(ButtonEvent.Pressed, function () {
+    sensors.searchRgbMinMax(sensors.color3)
+})
 // Вспомогательная функция проверки цвета спутника
 function CheckSetelliteColor () {
+    result_color = -1
+    color_samples = [-1]
+    control.timer1.reset()
     control.runInParallel(function () {
         music.playSoundEffect(sounds.informationAnalyze)
-        color_samples = [-1]
-        control.timer1.reset()
         while (control.timer1.millis() < 1000) {
             color_samples.push(CheckColor(true))
             pause(10)
         }
-        result_color = custom.mostFrequentNumber(color_samples)
-        if (result_color == ColorSensorColor.Brown) {
-            result_color = ColorSensorColor.Yellow
-        }
-        brick.printValue("current_color", current_color, 12)
     })
+    pause(10)
     ShakeSatellite()
+    pauseUntil(() => control.timer1.millis() > 1010)
+    result_color = custom.mostFrequentNumber(color_samples)
+    if (result_color == ColorSensorColor.Black) {
+        result_color = ColorSensorColor.White
+    }
+    if (result_color == ColorSensorColor.Brown) {
+        result_color = ColorSensorColor.Yellow
+    }
+    brick.printValue("result_color", result_color, 11)
     return result_color
 }
 // Вспомогательная функция, с помощью которой робот трясёт спутник, чтобы правильно прочитать значение цвета
@@ -194,15 +209,17 @@ function Claw (isOpen: boolean, speed: number, hold: boolean) {
 }
 function VoiceSatelliteColor (color: number) {
     if (color == ColorSensorColor.Blue) {
-        music.playSoundEffect(sounds.colorsBlue)
+        music.playSoundEffectUntilDone(sounds.colorsBlue)
     } else if (color == ColorSensorColor.Green) {
-        music.playSoundEffect(sounds.colorsGreen)
+        music.playSoundEffectUntilDone(sounds.colorsGreen)
     } else if (color == ColorSensorColor.Yellow) {
-        music.playSoundEffect(sounds.colorsYellow)
+        music.playSoundEffectUntilDone(sounds.colorsYellow)
     } else if (color == ColorSensorColor.Red) {
-        music.playSoundEffect(sounds.colorsRed)
+        music.playSoundEffectUntilDone(sounds.colorsRed)
     } else if (color == ColorSensorColor.White) {
-        music.playSoundEffect(sounds.colorsWhite)
+        music.playSoundEffectUntilDone(sounds.colorsWhite)
+    } else if (color == ColorSensorColor.Black) {
+        music.playSoundEffectUntilDone(sounds.colorsBlack)
     }
 }
 // Часть 4 - отвезти последний красный кубик0
@@ -226,7 +243,8 @@ function MoveLastRedSpaceGarbage () {
     pause(50)
     chassis.spinTurn(-90, 60)
     pause(50)
-    motions.lineFollowToCrossIntersection(AfterMotion.NoStop, params.lineFollowFourParams(80, 0.5, 0.5))
+    motions.lineFollowToDistance(200, AfterMotion.NoStop)
+    motions.lineFollowToCrossIntersection(AfterMotion.NoStop, params.lineFollowFourParams(70, 0.5, 0.5))
     chassis.rampLinearDistMove(30, 70, 150, 0, 150)
     pause(50)
     chassis.spinTurn(90, 60)
@@ -246,34 +264,33 @@ function MoveLastRedSpaceGarbage () {
     pause(50)
     chassis.spinTurn(180, 60)
 }
-let result_color = 0
 let color_samples: number[] = []
+let result_color = 0
 let cross = 0
-let current_color = 0
 let setellite_zone = 0
+let current_color = 0
 let color = 0
 let hsvl: number[] = []
 let rgb: number[] = []
-let sattelites: number[] = []
-brick.showImage(images.eyesCrazy2)
+brick.showImage(images.expressionsMouth1open)
 chassis.setChassisMotors(motors.mediumB, motors.mediumC, true, false)
 chassis.setSpeedRegulated(false)
-chassis.setSyncRegulatorGains(0.01, 0.001, 0.5)
+chassis.setSyncRegulatorGains(0.02, 0.001, 0.5)
 chassis.setWheelDiametr(62.4, MeasurementUnit.Millimeters)
 chassis.setBaseLength(170, MeasurementUnit.Millimeters)
 sensors.setNxtLightSensorsAsLineSensors(sensors.nxtLight1, sensors.nxtLight4)
-sensors.setLineSensorsRawRefValues(2312, 1632, 2448, 1916)
-sensors.setColorSensorMinRgbValues(sensors.color3, 5, 5, 6)
-sensors.setColorSensorMaxRgbValues(sensors.color3, 206, 227, 224)
+sensors.setLineSensorsRawRefValues(2272, 1660, 2504, 1950)
+sensors.setColorSensorMinRgbValues(sensors.color3, 3, 3, 3)
+sensors.setColorSensorMaxRgbValues(sensors.color3, 98, 87, 95)
 sensors.setHsvlToColorNumParams(sensors.color3, sensors.hsvlToColorNumParams(
-40,
-10,
+50,
+4,
 1,
 25,
 26,
 100,
-180,
-260
+190,
+270
 ))
 // Расстояние для прокатки после определения перекрёстка
 motions.setDistRollingAfterIntersection(40)
@@ -286,17 +303,10 @@ motions.setLineFollowConditionMaxErr(50)
 motions.setLineFollowLoopDt(1)
 levelings.setLineAlignmentOrPositioningLoopDt(1)
 motions.setLineFollowConditionMaxErr(50)
-motions.setSteeringAtSearchLineForLineFollowOneSensor(40)
+motions.setSteeringAtSearchLineForLineFollowOneSensor(50)
 motors.mediumD.setInverted(false)
 Claw(true, 50, true)
 sensors.color3.pauseUntilColorDetected(ColorSensorColor.None)
-sattelites = [
--1,
--1,
--1,
--1,
--1
-]
 brick.printValue("V", brick.batteryInfo(BatteryProperty.Voltage), 1)
 // Индикатор начала проги
 brick.setStatusLight(StatusLight.GreenPulse)
@@ -304,7 +314,12 @@ brick.setStatusLight(StatusLight.GreenPulse)
 brick.buttonEnter.pauseUntil(ButtonEvent.Bumped)
 // Индикатор выключаем
 brick.setStatusLight(StatusLight.Off)
-pause(10)
+if (false) {
+    while (true) {
+        CheckColor(true)
+        pause(10)
+    }
+}
 if (true) {
     TransferBattery()
 }
@@ -320,7 +335,7 @@ if (true) {
     pause(100)
     MoveLastRedSpaceGarbage()
 }
-if (false) {
+if (true) {
     pause(100)
     MoveSatellite()
 }
